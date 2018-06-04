0d5d67b29222a18ba3f7bddf2858e1de553e6741
tetsuyukimasuko <35556884+tetsuyukimasuko@users.noreply.github.com>
2018-06-04 12:03:02 +0900

    Update Manhattan_Transfer.py

--- Manhattan_Transfer.py
+++ Manhattan_Transfer.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     audio_file=request.files['wav']
-    hints=request.post['hints']
+    hints=request.form['hints']
 
     credentials = service_account.Credentials.from_service_account_file('My First Project-f92e7a809607.json')
     if credentials.requires_scopes:
